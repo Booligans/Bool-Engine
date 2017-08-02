7f129ceb91cf9e26eec93c9ef274704b1c3330d1
jcturing <juancvil@ucm.es>
2017-08-02 19:15:29 +0200

    Update Graphics.cpp

--- Graphics.cpp
+++ Graphics.cpp
@@ -4,6 +4,21 @@ void loadSprite(ALLEGRO_BITMAP * &image, std::string directory) {
 	image = al_load_bitmap(directory.c_str());
 }
 
+void loadSprite(std::vector<ALLEGRO_BITMAP*> &tImage, std::string directory, int width_num, int height_num) {
+	tImage.push_back(al_load_bitmap(directory.c_str()));
+	int width = al_get_bitmap_width(tImage[0]) / width_num;
+	int height = al_get_bitmap_height(tImage[0]) / height_num;
+	for (int i = 0; i < height_num; i++) {
+		for (int j = 0; j < width_num; j++) {
+			tImage.push_back(al_create_sub_bitmap(tImage[0], j * width, i * height, width, height));
+		}
+	}
+}
+
+void loadSprite(std::vector<ALLEGRO_BITMAP*> &tImage, std::string directory) {
+
+}
+
 bool saveSprite(ALLEGRO_BITMAP * &image, std::string directory) {
 	return al_save_bitmap(directory.c_str(), image);
 }
@@ -49,7 +64,16 @@ void drawSprite(ALLEGRO_BITMAP *image, float posx, float posy, ALLEGRO_COLOR tin
 void showScreen() {
 	al_flip_display();
 }
+
 void destroySprite(ALLEGRO_BITMAP * &image) {
 	al_destroy_bitmap(image);
 	image = nullptr;
+}
+
+void destroySprite(std::vector <ALLEGRO_BITMAP*> &tImage) {
+	for (size_t i = tImage.size(); i > 0; i--) {
+		al_destroy_bitmap(tImage[i - 1]);
+		tImage[i - 1] = nullptr;
+		tImage.pop_back();
+	}
 }
