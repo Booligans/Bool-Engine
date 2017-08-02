db9ac10e3aed38fecb177caaed80c5b2720a5276
jcturing <juancvil@ucm.es>
2017-08-03 01:25:44 +0200

    Update Control.cpp

--- Control.cpp
+++ Control.cpp
@@ -1,14 +1,22 @@
 #include "Control.h"
 
-void initializeEvents(ALLEGRO_DISPLAY *display, ALLEGRO_TIMER * &timer, ALLEGRO_EVENT_QUEUE * &event_queue, bool use_timer, bool use_display, bool use_keyboard, bool use_mouse) {
-	if (use_timer) al_register_event_source(event_queue, al_get_timer_event_source(timer));
+void initializeEvent(ALLEGRO_DISPLAY *display, ALLEGRO_EVENT_QUEUE * &event_queue, bool use_display, bool use_keyboard, bool use_mouse) {
+	event_queue = al_create_event_queue();
 	if (use_display) al_register_event_source(event_queue, al_get_display_event_source(display));
 	if (use_keyboard) al_register_event_source(event_queue, al_get_keyboard_event_source());
 	if (use_mouse) al_register_event_source(event_queue, al_get_mouse_event_source());
 }
 
-bool createDisplay(ALLEGRO_DISPLAY * &display, int const& screenHeight, int const& screenWidth, int const& screen_x_position, int const& screen_y_position, std::string screenName, ScreenType const& screenType) {
-	bool create = true;
+void waitEvent(ALLEGRO_EVENT_QUEUE * &event_queue, ALLEGRO_EVENT &events) {
+	al_wait_for_event(event_queue, &events);
+}
+
+void destroyEvent(ALLEGRO_EVENT_QUEUE * &event_queue) {
+	al_destroy_event_queue(event_queue);
+	event_queue = nullptr;
+}
+
+void createDisplay(ALLEGRO_DISPLAY * &display, int const& screenHeight, int const& screenWidth, int const& screen_x_position, int const& screen_y_position, std::string screenName, ScreenType const& screenType) {
 	int screenFlag = standard;
 
 	switch (screenType) {
@@ -23,7 +31,38 @@ bool createDisplay(ALLEGRO_DISPLAY * &display, int const& screenHeight, int cons
 	display = al_create_display(screenHeight, screenWidth);
 	al_set_window_position(display, screen_x_position, screen_y_position);
 	al_set_window_title(display, screenName.c_str());
-	if (!display)	create = false;
+}
 
-	return create;
+void destroyDisplay(ALLEGRO_DISPLAY * &display) {
+	al_destroy_display(display);
+	display = nullptr;
+}
+
+void createTimer(ALLEGRO_TIMER * &timer, const double FPS) {
+	timer = al_create_timer(1 / FPS);
+}
+
+void registerTimer(ALLEGRO_EVENT_QUEUE * &event_queue, ALLEGRO_TIMER *timer) {
+	al_register_event_source(event_queue, al_get_timer_event_source(timer));
+}
+
+void startTimer(ALLEGRO_TIMER *timer) {
+	al_start_timer(timer);
+}
+
+void stopTimer(ALLEGRO_TIMER *timer) {
+	al_stop_timer(timer);
+}
+
+void destroyTimer(ALLEGRO_TIMER * &timer) {
+	al_destroy_timer(timer);
+	timer = nullptr;
+}
+
+void getKeyboard(ALLEGRO_KEYBOARD_STATE &keyState) {
+	al_get_keyboard_state(&keyState);
+}
+
+bool keyPressed(ALLEGRO_KEYBOARD_STATE &keyState, keycodes key) {
+	return al_key_down(&keyState, key);
 }
