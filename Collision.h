5909a56393535119ab5d1eb60a3458a91d361b0b
jcturing <juancvil@ucm.es>
2017-08-02 19:12:24 +0200

    Update Collision.h

--- Collision.h
+++ Collision.h
@@ -8,10 +8,13 @@
 
 //Returns true if the box of the sprite collides with another one. Recommended for rectangles | PARAMETERS: First sprite, position x, position y, Second sprite, second position x, second position y.
 bool collisionBox(ALLEGRO_BITMAP *image, float posx, float posy, ALLEGRO_BITMAP *sec_image, float sec_posx, float sec_posy);
-//DONT USE! NOT DONE. Returns true if the distance calculated between two sprites collides. Recommended for circles | PARAMETERS: First sprite, position x, position y, Second sprite, second position x, second position y.
+//Returns true if the distance calculated between two sprites collides. Recommended for circles with center in the middle | PARAMETERS: First sprite, position x, position y, Second sprite, second position x, second position y.
 bool collisionDistance(float posx, float posy, float radius, float sec_posx, float sec_posy, float sec_radius);
-//DONT USE! NOT DONE. Returns true if... Recommended for perfect collisions but it might be slow. | PARAMETERS: First sprite, position x, position y, Second sprite, second position x, second position y.
-bool collisionPixel();
-
+//Returns true if... Recommended for perfect collisions but it might be slow. | PARAMETERS: First sprite, position x, position y, Second sprite, second position x, second position y.
+bool collisionPixel(ALLEGRO_BITMAP *image, float posx, float posy, ALLEGRO_BITMAP *sec_image, float sec_posx, float sec_posy);
+//Returns the larger of the two numbers.
+float max(float x, float y);
+//Returns the lower of the two numbers.
+float min(float x, float y);
 
 #endif
