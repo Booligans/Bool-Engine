4b9e31a3ac6f3cfa57cbacb579a5e6d77dfe198c
jcturing <juancvil@ucm.es>
2017-08-30 17:08:19 +0200

    Update Graphics.h

--- Graphics.h
+++ Graphics.h
@@ -5,20 +5,21 @@
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_image.h>
 #include <allegro5/allegro_primitives.h>
+#include <allegro5/allegro_ttf.h>
+#include <allegro5/allegro_font.h>
 
 //Other libraries needed
 #include <string>
 #include <vector>
 
-//Constants and types needed
-struct tPixelRegion {
-	int width;
-	int height;
-};
+//TYPES
+enum textType {ALLIGN_LEFT, ALLIGN_CENTRE, ALLIGN_RIGHT};
 
-//LOADING AND SAVING A SPRITE
+//SPRITES
+
+//Loading & Saving sprites
 //Loads a normal sprite | PARAMETERS: Sprite, file directory.
-void loadSprite(ALLEGRO_BITMAP * &image, std::string directory);
+void loadSprite(ALLEGRO_BITMAP * &image, const std::string file);
 //Loads a sprite animation with same sizes. The first position of the vector will be the whole sprite sheet. | PARAMETERS: Vector of sprites, file directory, number of sprites per line, number of lines.
 void loadSprite(std::vector<ALLEGRO_BITMAP*> &tImage, std::string directory, int width_num, int height_num);
 //Loads a sprite animation with different sizes (needs to have a line separator). The first position of the vector will be the whole sprite sheet. | PARAMETERS: Vector of sprites, file directory, vector with the number of pixel per sprite. NOT DONE
@@ -26,7 +27,7 @@ void loadSprite(std::vector<ALLEGRO_BITMAP*> &tImage, std::string directory);
 //Saves a sprite. Returns true if the image has been saved successfully | PARAMETERS: Sprite, file directory.
 bool saveSprite(ALLEGRO_BITMAP * &image, std::string directory);
 
-//DRAWING A SPRITE | OPTIONS: Tinted, Rotated, Scaled and Combinations.
+//Drawing sprites | OPTIONS: Tinted, Rotated, Scaled and Combinations.
 //Draws a standard sprite | PARAMETERS: Sprite, Print position.
 void drawSprite(ALLEGRO_BITMAP *image, float posx,  float posy);
 //Draws a tinted sprite | PARAMETERS: Sprite, Print position, color (multiplies all the colors of the sprite by the given color). 
@@ -44,12 +45,31 @@ void drawSprite(ALLEGRO_BITMAP *image, float posx, float posy, float centerx, fl
 //Loads and draws a tinted, rotated and scaled sprite | PARAMETERS: Sprite, Print position, color, center of rotation, angle, scale of width and scale of height.
 void drawSprite(ALLEGRO_BITMAP *image, float posx, float posy, ALLEGRO_COLOR tint, float centerx, float centery, float angle, float swidth, float sheight);
 
-//IMPORTANT FUNCIONS FOR A SPRITE.
-//Clears the screen and then shows in the displays the backbuffer (everything drawed).
-void showScreen();
-//Destroys the sprite, freeing al the memmory and resources used by it, also lets the pointer to nullptr. PARAMETERS: Sprite.
+//Destroying sprites
+//Destroys the sprite, freeing al the memmory and resources used by it, also lets the pointer to nullptr | PARAMETERS: Sprite.
 void destroySprite(ALLEGRO_BITMAP * &image);
 //Destroys a vector of sprites, freeing all the memmory and resources used by all of them, lets all the pointers to nullptr. PARAMETERS: Vector of sprites.
 void destroySprite(std::vector <ALLEGRO_BITMAP*> &tImage);
+
+//FONTS
+
+//Loading fonts
+//Loads a font | PARAMETERS: font pointer, name, size.
+void loadFont(ALLEGRO_FONT * &font, std::string font_name, int size);
+
+//Drawing font | PARAMETERS:  font pointer, color of the text, position x and y, flag(LEFT, CENTER, RIGHT) and the text to print.
+void drawFont(ALLEGRO_FONT * font, ALLEGRO_COLOR text_color, int x, int y, textType text_type, std::string text);
+
+//Destroying fonts
+//Destroys the font, freeing all the memmory and resources used by it, also lets the pointer to nullptr | PARAMETERS: font pointer.
+void destroyFont(ALLEGRO_FONT * &font);
+
+
+//OTHER GRAPHIC FUNCTIONS
+//Shows in the displays the backbuffer (everything drawed) and then clears the entire backbuffer.
+void showScreen(ALLEGRO_COLOR back_color);
+//Shows in the displays the backbuffer (everything drawed) and then clears the backbuffer of the clipping rectangle.
+void showScreen(ALLEGRO_COLOR back_color, int x, int y, int width, int height);
+
 
 #endif
